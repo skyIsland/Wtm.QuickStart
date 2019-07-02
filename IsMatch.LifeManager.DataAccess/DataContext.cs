b8ff31bf8375bea56f41ab2a93924caa18019bdd
IsMatch <ismatch@qq.com>
2019-07-02 14:15:20 +0800

    Update.

---
+++ IsMatch.LifeManager.DataAccess/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WalkingTec.Mvvm.Core;
+
+namespace IsMatch.LifeManager.DataAccess
+{
+    public class DataContext : FrameworkContext
+    {
+        public DataContext(string cs, DBTypeEnum dbtype)
+             : base(cs, dbtype)
+        {
+        }
+
+    }
+}
